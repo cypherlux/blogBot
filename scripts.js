0adc97b307e81aaf283410d921ac2777b0a976ea
cypherlux <89822852+cypherlux@users.noreply.github.com>
2022-11-25 09:57:33 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,11 +13,11 @@ const fs = require ('fs/promises')
 async function start() {
     const browser = await puppeteer.launch({});
     const page = await browser.newPage();
-    await page.goto("https://zenhabits.net/archives/"); //change this URL to change the page that I scrape
+    await page.goto(""); //change this URL to change the page that I scrape
  
-    //creates an array of all elements with ".post-title" in a new txt document
+    //creates an array of all elements with "THISELEMENT" in a new txt document
     const names = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".post-title")).map(x => x.textContent)
+        return Array.from(document.querySelectorAll("")).map(x => x.textContent)
     })
     await fs.writeFile("names.txt", names.join("\r\n")) //creates a new file w/ a certain name
 
